bd8546fb0d6bbe03812e919396f5de29f754ba29
DRdarwin <31813699+DRdarwin@users.noreply.github.com>
2024-06-02 17:14:45 +0300

    Add files via upload

---
+++ types/contact.d.ts
@@ -0,0 +1,9 @@
+export type Contact = {
+    wa_id: number,
+    created_at: string | null,
+    last_message_at: string | null,
+    profile_name: string | null,
+    tags: string[] | null,
+    is_current?: boolean,
+    in_chat: boolean,
+}
